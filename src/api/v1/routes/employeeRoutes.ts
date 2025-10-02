6dbdd63254cccf8c3429efe596536b10a7e3e8ce
harsh pandya <hpandya38@rrc.ca>
2025-10-01 20:57:24 -0500

    Added and change in routes file.'

--- src/api/v1/routes/employeeRoutes.ts
+++ src/api/v1/routes/employeeRoutes.ts
@@ -5,7 +5,7 @@ const router = Router();
  
 router.post('/', ctrl.createEmployee);
 router.get('/', ctrl.getAllEmployees);
-router.get('/department/:department', ctrl.getEmployeesByDepartment);
+router.get('/:department', ctrl.getEmployeesByDepartment);
 router.get('/:id', ctrl.getEmployeeById);
 router.put('/:id', ctrl.updateEmployee);
 router.delete('/:id', ctrl.deleteEmployee);
