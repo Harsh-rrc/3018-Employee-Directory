bffd0b1aa5dc82441a26570faaa6359d28de9f50
harsh pandya <hpandya38@rrc.ca>
2025-10-12 22:06:26 -0500

    Add Joi validation middleware to ensure incoming request data is validated before controllers.

---
+++ src/api/v1/middleware/validationMiddleware.ts
@@ -0,0 +1,16 @@
+// src/middleware/validate.ts
+import { Request, Response, NextFunction } from "express";
+import { Schema } from "joi";
+
+export const validate = (schema: Schema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res.status(400).json({
+        message: "Validation error",
+        details: error.details.map((d) => d.message),
+      });
+    }
+    next();
+  };
+};
