0e47dae547dc72884d0ea7ea9f65c91879780e35
harsh pandya <hpandya38@rrc.ca>
2025-10-12 22:08:18 -0500

    Add Joi validation middleware changes.

--- src/api/v1/middleware/validationMiddleware.ts
+++ src/api/v1/middleware/validationMiddleware.ts
@@ -1,10 +1,15 @@
-// src/middleware/validate.ts
+// this middleware validates request bodies against a given Joi schema.
 import { Request, Response, NextFunction } from "express";
 import { Schema } from "joi";
 
+/**
+ * 
+ * @param schema 
+ * @returns 
+ */
 export const validate = (schema: Schema) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body, { abortEarly: false });
+    const { error } = schema.validate(req.body, { abortEarly: false });   // abortEarly: false to get all errors
     if (error) {
       return res.status(400).json({
         message: "Validation error",
