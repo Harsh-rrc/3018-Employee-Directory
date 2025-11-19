6e1738620b002132002704523fd1bacdd1d45bcb
harsh pandya <hpandya38@rrc.ca>
2025-11-18 22:25:09 -0600

    UPdates the files.

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,10 +1,13 @@
+// src/docs/index.ts
 import express from "express";
 import employeeRoutes from "../api/v1/routes/employeeRoutes";
 import branchRoutes from "../api/v1/routes/branchesRoutes";
 
+// Initialize Express app
 const app = express();
 const PORT = 3000;
 
+// Middleware
 app.use(express.json());
 app.use("/api", employeeRoutes);
 app.use('/api/branches',branchRoutes);
--- src/scripts/generate-openapi.ts
+++ src/scripts/generate-openapi.ts
@@ -1,6 +1,8 @@
+// src/scripts/generate-openapi.ts
 import fs from "fs";
 import { generateSwaggerSpec } from "../config/swaggerOptions";
 
+// Generate OpenAPI spec and write to openapi.json
 const specs = generateSwaggerSpec();
 fs.writeFileSync("./openapi.json", JSON.stringify(specs, null, 2));
 console.log("OpenAPI spec generated!");
