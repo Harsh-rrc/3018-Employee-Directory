9b73e333c5496945e3c296664e707507fdfa3a5b
harsh pandya <hpandya38@rrc.ca>
2025-11-18 22:29:09 -0600

    Update the server file.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 // src/server.ts    
 import app from "./app";
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
+});
